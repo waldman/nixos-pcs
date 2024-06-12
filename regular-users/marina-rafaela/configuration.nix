f14a43ebc47a93bbf05596d6daaabc061210e6f3
Leon Waldman <leon@waldman.io>
2024-06-11 21:48:30 -0300

    minor fix

--- regular-users/marina-rafaela/configuration.nix
+++ regular-users/marina-rafaela/configuration.nix
@@ -97,8 +97,6 @@
       gnome.gnome-terminal
       gnome.gnome-tweaks
       google-chrome
-      mc
-      neovim
       zoom-us
     ];
   };
@@ -132,7 +130,9 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    mc
+    neovim
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
 
